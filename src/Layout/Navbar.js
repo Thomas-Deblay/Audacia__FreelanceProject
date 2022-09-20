fc7bcfb73d4339a2e9d80cf38ad6d2f3ec9b65a9
Thomas-Deblay <thomasdeblay@pc-10-148.customer.ask4.lan>
2022-09-21 00:29:35 +0200

    changement de limage logo navbar for padding top again

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -10,7 +10,7 @@ import Scrollspy from 'react-scrollspy';
 
 //import images
 import logoLight from '../assets/images/logoAudiaciaLight.png';
-import logoDark from '../assets/images/logoHome4.png';
+import logoDark from '../assets/images/logoHome3.png';
 import { Link } from 'react-router-dom';
 
 const NavBar = ({ isDark }) => {
@@ -54,7 +54,7 @@ const NavBar = ({ isDark }) => {
                     src={logoDark}
                     className="logo-light"
                     alt=""
-                    height="130"
+                    height="70"
                   />
                   <img
                     src={logoDark}
